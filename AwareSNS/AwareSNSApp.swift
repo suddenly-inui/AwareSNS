8445c990780eb53a80e7f6a64f927f86b894b7dd
suddenly_inui <inuiyuki0904@gmail.com>
2023-10-02 16:15:53 +0900

    Initial Commit

---
+++ AwareSNS/AwareSNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwareSNSApp.swift
+//  AwareSNS
+//
+//  Created by Yuki Inui on 2023/10/02.
+//
+
+import SwiftUI
+
+@main
+struct AwareSNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
